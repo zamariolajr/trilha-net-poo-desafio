c7ef9748bec04092203e11aa520de767e130972e
zamariolajr <zamariolajr@gmail.com>
2025-07-10 20:19:37 -0300

    Implementação dos pontos solicitados no projeto.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp) => Console.WriteLine($"Instalando applicativo Nokia: {nomeApp}");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia!!!");
+Smartphone cel1 = new Nokia(numero: "32518188", modelo: "tijorola", imei: "123456789", memoria: 12);
+cel1.Ligar();
+cel1.InstalarAplicativo("X");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone!!!");
+Smartphone cel2 = new Iphone(numero: "32519877", modelo: "xv", imei: "178923456", memoria: 128);
+cel2.Ligar();
+cel2.InstalarAplicativo("Tradingview");
